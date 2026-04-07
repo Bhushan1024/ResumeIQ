6afc7a55346bbd4fef7ac9d1f4ea0cf537519562
Bhushan Chavan <bhushan.chavan@airefined.com>
2026-04-07 11:01:06 +0530

    Update LLM model

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     
-    LLM_MODEL: str = "llama3.2:3b"      # Change if you pulled a different model or we can use llama3.2:3b or "qwen3:8b"
+    LLM_MODEL: str = "llama3.2:3b"      # Change if you pulled a different model or we can use "llama3.2:3b" or "qwen3:8b" # Changed model to "llama3.2:3b"  as previous model "qwen3:8b" giving slow responses
     LLM_TEMPERATURE: float = 0.0        # Low for consistent extraction
     LLM_MAX_TOKENS: int = 2048
     
